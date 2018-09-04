988b0049c7fd288de31e4797185ac5b97d3a2a22
Unknown <jhobeck376@west-mec.org>
2018-09-04 15:28:37 -0700

    Initial Update 1
    Began coding for the postal code section.

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 /*  Project 01_11_04
 
-    Author: 
-    Date:   
+    Author: Jacob Hobeck
+    Date:   9.4.18
 
     Filename: script.js
 */
 
 "use strict";
 
+// global variables
+var httpRequest = false;
+
+function checkInput() {
+    var zip = document.getElementById("zip").value;
+    if (zip.length === 5) {
+        getLocation();
+    } else {
+        document.getElementById("city").value = "";
+        document.getElementById("state").value = "";
+    }
+}
+
+// creating event listeners
+var zip = document.getElementById("zip");
+if (zip.addEventListener) {
+    zip.addEventListener("keyup", checkInput, false);
+} else if (zip.attachEvent) {
+    zip.attachEvent("onkeyup", checkInput);
+}
